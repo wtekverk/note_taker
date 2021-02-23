88ae202f2f73d8dd64cfe753d56947e3accec8ba
Wendy Tekverk <wtekverk@gmail.com>
2021-02-23 14:05:14 -0700

    adding comments and refixing a bug with delete and style

--- routes/api.js
+++ routes/api.js
@@ -1,39 +1,26 @@
-var noteData = require("../db/db.json");
+var notesData = require("../db/db.json");
 
 module.exports = function(app) {
   
-  /* Read the `db.json` file and return all saved notes as JSON.*/
+//this gets all information from JSON db file 
   app.get("/api/notes", function(req, res) {
-    res.json(noteData);
+    res.json(notesData);
   });
 
-  /* Receive a new note to save on the request body, 
-  add it to the `db.json` file, and then return the new note 
-  to the client.  */
+
+
+
   app.post("/api/notes", function(req, res) {
-    let received = req.body;
-    received.id = noteData.length.toString();
-    noteData.push(received);
-    res.json(received);
+
+
+    let response = req.body;
+    response.id = notesData.length.toString();
+
+    notesData.push(response);
+    res.json(response);
   })
 
-  /* Delete the note corresponding to that ID, and re-number
-  all notes to have an ID corresponding to their index.
-  This solution was not elegant as JSONs cannot have integers
-  with a leading 0, including 0; as such, I had to cast the
-  note position to a string. A better solution probably
-  exists elsewhere*/
-  app.delete("/api/notes/:id", function(req,res) {
-    let deleteID = req.params.id;
-    for (var i = 0; i < noteData.length; i++) {
-      if (noteData[i].id == deleteID) {
-        noteData.splice(i, 1);
-        break;
-      }
-    }
-    for (var i = 0; i < noteData.length; i++) {
-      noteData[i].id = i.toString();
-    }
-    return res.json(true);
-  })
+  //delete functionality should go here when I figure out how to do that
+  
+  
 };
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-
+//attaching routs 
 require("./routes/api")(app);
 require("./routes/html")(app);
 
